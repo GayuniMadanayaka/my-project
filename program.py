00178b1d74f9f1cb20fcbc5bd4cbf6e495c5bb09
sasi1219 <sasinidr@gmail.com>
2024-05-19 20:18:20 +0530

    Update program.py

--- program.py
+++ program.py
@@ -10,5 +10,13 @@ if __name__ == "__main__":
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}.")
 
+    num3 = 6
+    num4 = 9
+    result = add_numbers(num3, num4)
+    print(f"The sum of {num3} and {num4} is {result}.")
+
+age = 12
+print(age)
+
 value = 10
 print(value)
